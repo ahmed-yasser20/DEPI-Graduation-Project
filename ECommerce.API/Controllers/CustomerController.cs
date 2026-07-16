4c5bda78b60c02a2ccf52afa8210b1965568bb30
Kim0-k <mohamedkimo1973@yahoo.com>
2026-07-16 23:11:11 +0300

    Quick file fixes

--- ECommerce.API/Controllers/CustomerController.cs
+++ ECommerce.API/Controllers/CustomerController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 
-namespace ECommerce.API
+namespace ECommerce.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
